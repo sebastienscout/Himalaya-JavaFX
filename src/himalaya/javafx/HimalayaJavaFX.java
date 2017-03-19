d157190c898cc00b742fe52c3bb83f75985116b6
sebastienscout <schouteeten.sebastien@gmail.com>
2017-03-19 14:50:45 +0100

    ADD gitignore + test image

--- src/himalaya/javafx/HimalayaJavaFX.java
+++ src/himalaya/javafx/HimalayaJavaFX.java
@@ -6,27 +6,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
  * @author sebastien
  */
 public class HimalayaJavaFX extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Dire 'Bonjour'");
-        
+
         btn.setOnAction((ActionEvent event) -> {
             System.out.println("Bonjour !");
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        StackPane pane = new StackPane();
         
-        Scene scene = new Scene(root, 300, 250);
-        
+        float width = 1840 / 2;
+        float height = 1298 / 2;
+
+        Image background = new Image("file:resources/board.jpg", width, height, true, true);
+        ImageView iv = new ImageView(background);
+
+        pane.getChildren().add(iv);
+        pane.getChildren().add(btn);
+
+        Scene scene = new Scene(pane, width, height);
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -38,5 +48,5 @@ public class HimalayaJavaFX extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
