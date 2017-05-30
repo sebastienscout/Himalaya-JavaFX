e8dea72e87f4c5c0fbf2a404998ed18c9e02e05e
Alexis Jouin <alexis.jouin@live.fr>
2017-05-30 10:37:49 +0200

    RESIZE GAME

--- src/ihm/MenuFXMLController.java
+++ src/ihm/MenuFXMLController.java
@@ -7,7 +7,6 @@ import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.FlowPane;
