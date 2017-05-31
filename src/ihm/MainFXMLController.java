6eb350343f71f74a2fb7d73cf4c03993ec914833
Alexis Jouin <alexis.jouin@live.fr>
2017-05-31 13:14:53 +0200

    FIX : for play human

--- src/ihm/MainFXMLController.java
+++ src/ihm/MainFXMLController.java
@@ -93,8 +93,8 @@ public class MainFXMLController implements Initializable, ControlledScreen {
 
         playTurn.setDisable(true);
 
-        playG.run();
         playG.setBackground(background);
+        playG.run();
 
         playG.getBoard().prepareActions();
 
